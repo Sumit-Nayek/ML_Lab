8a5cd9bac82de73ab9ec558180f956418559161c
Sumit Nayek <63765654+Sumit-Nayek@users.noreply.github.com>
2026-06-13 12:43:31 +0530

    Clean up comments in Solution.py
    Removed unnecessary comment before model training.

--- Lab10/Solution.py
+++ Lab10/Solution.py
@@ -16,7 +16,7 @@ class_names = iris.target_names
 # Split the dataset into training and testing sets (80% train, 20% test)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Create and train the Logistic Regression model
+
 # Using 'multinomial' for multi-class classification with 'lbfgs' solver
 model = LogisticRegression(max_iter=200, multi_class='multinomial', solver='lbfgs', random_state=42)
 model.fit(X_train, y_train)
